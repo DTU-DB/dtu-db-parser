6db2e925894197a7ee8678ea0870199380f4abf8
unknownblueguy6 <unknownblueguy6@gmail.com>
2023-05-14 00:24:56 +0530

    refactor: add more files for testing, fix console output

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,31 @@
 import { PDFParser } from "./PDFParser.js"
-import * as fs from "fs";
 
 
 async function main(){
-    let pj = new PDFParser("E22_BTECH_VI.pdf")
-    await pj.readPDF();
-    // console.log('readpdf')
-    // console.log(pj.pages[0])
-    await pj.parsePages()
-    // pj.parsePageNew(pj.pages[0])
-    console.log(pj.students.length)
-    // fs.writeFile('../testnew.json', JSON.stringify(pj.students), (err) => {
-    //     if (err) throw err
-    // });
-    let total = 0
-    pj.students.forEach((student, i) => {
-        if (student.semester.subjects.some(sub => sub.grade === "")){
-            console.log(student.name);
-            total++;
-        }
-    })
-    console.log(total);
-
+    const files = "E20_BTECH_II_1107.pdf, E21_BTECH_IV_1251.pdf, E22_BTECH_VI.pdf, O19_BTECH_1_1015.pdf, O20_BTECH_1183.pdf, O21_BTECH_V.pdf".split(", ")
+    for(const file of files){
+        
+        let pj = new PDFParser(`pdfs/${file}`)
+        
+        await pj.readPDF();
+        await pj.parsePages();
+        
+        let total = 0
+        
+        console.log(file)
+        console.log("Total number of students = ", pj.students.length)
+        console.log("Students with empty grade : ")
+        
+        pj.students.forEach((student, i) => {
+            if (student.semester.subjects.some(sub => sub.grade === "")){
+                console.log(i, student.rollno, student.name);
+                total++;
+            }
+        })
+        
+        console.log("Number of students with empty grade =", total);
+        console.log()
+    };
 }
 
 main()
