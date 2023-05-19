6d6021c0cf45b2cde45baa49319c38ccd8fc03c1
unknownblueguy6 <unknownblueguy6@gmail.com>
2023-05-20 02:31:05 +0530

    refactor: fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,7 @@
-import { StudentParser } from "./parsers/index.js";
-// import { writeFileSync } from "fs";
+import * as Parsers from "./parsers/index.js"
+import * as StudentModels from "./models/index.js"
 
-async function main(){
-    const files = "E20_BTECH_II_1107.pdf, E21_BTECH_IV_1251.pdf, E22_BTECH_VI.pdf, O19_BTECH_1_1015.pdf, O20_BTECH_1183.pdf, O21_BTECH_V.pdf".split(", ")
-    for(const file of files){
-        
-        const pj = new StudentParser(`pdfs/${file}`);
-        
-        await pj.readPDF();
-        await pj.parsePages();
-        // writeFileSync(`generated_files/${file.slice(0, -4)}.json`, JSON.stringify(pj.students))
-        
-        let total = 0
-        
-        console.log(file)
-        console.log("Total number of students = ", pj.students.length)
-        console.log("Students with empty grade : ")
-        
-        pj.students.forEach((student, i) => {
-            if (student.semester.subjects.some(sub => sub.grade === "")){
-                console.log(i, student.rollno, student.name);
-                total++;
-            }
-        })
-        
-        console.log("Number of students with empty grade =", total);
-        console.log()
-    };
+export {
+    Parsers,
+    StudentModels
 }
-
-await main()
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
-export { Student } from "./student.js";
+export { Department, departmentCodeToDepartmentName } from "./dept.js"
 export { EMPTY_GRADE, PASSING_GRADES, FAILING_GRADES, VALID_GRADES} from "./grade.js"
-export { departmentCodeToDepartmentName } from "./dept.js"
+export { Semester, SemesterSubject } from "./semester.js"
+export { Student } from "./student.js";
